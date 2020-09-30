c41779b0b63706e2994cc8dbca4c6340c6ae10d3
Akanksha <Akanksha@DESKTOP-EEPHO35>
2020-09-30 15:59:36 +0530

    CRUD for messages

--- src/main/java/com/examples/resteg/messenger1/resources/MessageResource.java
+++ src/main/java/com/examples/resteg/messenger1/resources/MessageResource.java
@@ -2,8 +2,13 @@ package com.examples.resteg.messenger1.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,14 +16,42 @@ import com.examples.resteg.messenger1.model.Message;
 import com.examples.resteg.messenger1.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
+	}
+	
+	
+	@GET
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId){
+		return messageService.getMessage(messageId);
+//		return "message id received  " + messageId;
+	}
+	
+	@POST
+		public Message addMessage(Message message) {
+			return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+		public Message updateMessage(@PathParam("messageId") long messageId , Message message) {
+			message.setId(messageId);
+			return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public String deleteMessage(@PathParam("messageId") long messageId){
+		 messageService.removeMessage(messageId);
+		 return "Message Deleted ";
 	}
 }
--- src/main/java/com/examples/resteg/messenger1/service/MessageService.java
+++ src/main/java/com/examples/resteg/messenger1/service/MessageService.java
@@ -2,19 +2,43 @@ package com.examples.resteg.messenger1.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.examples.resteg.messenger1.model.Message;
 
+import co.examples.resteg.messenger1.database.DatabaseClass;
+
 public class MessageService {
 
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1 , "Hello World", "Akanksha"));
+		messages.put(2L, new Message(2 , "Hey There", "Ajinkya"));
+	}
 	
 	public List<Message> getAllMessages(){
 		
-		Message m1 = new Message(1L,"HEllo there", "AKanksha");
-		Message m2 = new Message(1L,"hi everyone", "Priya");
-		List<Message> list= new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
+		return new ArrayList<Message>(messages.values());
+		
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	public Message addMessage (Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	public Message updateMessage(Message message) {
+		if(message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 }
